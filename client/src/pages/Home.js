fa55a3fb36c1f862e7b0d83b24cb4ee23078a48f
devpatelio <hello.devpatel@gmail.com>
2024-01-02 22:05:00 -0500

    added front-end filtering search

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,26 +3,22 @@ import { server_url } from "../constants";
 // import ResourceCard from "../Card.js" 
 
 function Home() {
-  // async function im_a_teapot() {
-  //   try {
-
-  //     const response = await fetch(`${server_url}/sample-route`, {
-  //       method: "POST",
-  //     });
-
-  //     if (!response.ok) {
-  //       throw new Error("Network response was not ok");
-  //     }
-
-  //     const data = await response.json();
-  //     console.log(data);
-  //   } catch (error) {
-  //     console.error("There was a problem with the fetch operation:", error);
-  //   }
-  // }
+  
+    // render(
+    //   <form className="text-2xl flex flex-col h-screen items-left">
+    //     <label htmlFor="search_filter">Select Resource Type</label><select id="search_filter" className="form-control">
+    //         <option value="all">All</option>
+    //         <option value="partnership">Partnership</option>
+    //         <option value="dataset">Dataset</option>
+    //         <option value="guide">Guide</option>
+    //     </select><br></br><br></br>
+    //     <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded" onClick={props.onClick}>Submit</button>
+    //   </form>
+    // )
 
   
-  const [resources, setResources] = useState([]);
+  const [resourcesAll, setResources] = useState([]);
+  // console.log(resourcesGuides)
 
   const allResources = async() => {
     try{
@@ -38,6 +34,47 @@ function Home() {
     allResources();
   }, []);
 
+  const [allresources, selectAllResources] = useState(true);
+  const toggleDefault = () => {
+    selectAllResources(!allresources);
+    selectGuide(guide);
+    selectPartnership(partnership);
+    selectDataset(false);
+  };
+
+  const [guide, selectGuide] = useState(false);
+  const toggleGuides = () => {
+    selectGuide(!guide);
+    selectAllResources(false);
+    selectPartnership(false);
+    selectDataset(false);
+  };
+  const resourcesGuides = resourcesAll.filter(function(item){
+    return item.class="guide";
+  });
+
+  const [partnership, selectPartnership] = useState(false);
+  const togglePartnership = () => {
+    selectGuide(false);
+    selectAllResources(false);
+    selectPartnership(true);
+    selectDataset(false);
+  };
+  const resourcesPartnerships = resourcesAll.filter(function(item){
+    return item.class="partnership";
+  });
+
+  const [dataset, selectDataset] = useState(false);
+  const toggleDataset = () => {
+    selectGuide(false);
+    selectAllResources(false);
+    selectPartnership(false);
+    selectDataset(true);
+  };
+  const resourceDatasets = resourcesAll.filter(function(item){
+    return item.class="dataset";
+  });
+
   // console.log(resources);
 
   return (
@@ -49,50 +86,54 @@ function Home() {
       <button onClick={event => window.location.href='/new'} className="text-2xl border-black border-2 p-2 mt-3 w-64 hover:bg-lime-100">
         Create New Resource
       </button>
-    {/* <div>
-      <div className="text-2xl p-20 mr-5 flex-wrap flex-row" style={{ display: "flex", flexDirection: "row" }}>
-        {resources.map(resource_entry => (
-          <div key={resource_entry.resource_id} class="w-200 h-100 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <a href="#">
-              <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-          </a>
-          <div className="p-6">
-              <a href="#">
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{resource_entry.title}</h5>
-              </a>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 whitespace-normal break-words"> {resource_entry.description} </p>
-              <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                  {resource_entry.class}
-                  <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                  </svg>
-              </a>
-          </div>
-      </div>
-        ))}
-      </div> */}
 
-<div className="text-2xl p-20 mr-5 grid grid-cols-3 gap-8">
-  {resources.map((resource_entry) => (
-    <div key={resource_entry.resource_id} className="w-80 h-96 bg-white border border-gray-200 rounded-lg shadow overflow-hidden dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
-        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-      </a>
-      <div className="p-6">
-        <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{resource_entry.title}</h5>
-        </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 whitespace-normal break-words">{resource_entry.description}</p>
-        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          {resource_entry.class}
-          <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-          </svg>
-        </a>
+    <div>
+      <button onClick={ toggleDefault } className="bg-orange-500 hover:bg-white-300 text-white py-2 px-4 rounded">Default</button>
+      {allresources ? (
+        <div className="text-2xl p-20 mr-5 grid grid-cols-3 gap-8">
+        {resourcesAll.map((resource_entry) => (
+          <div key={resource_entry.resource_id} className="w-80 h-96 bg-white border border-gray-200 rounded-lg shadow overflow-hidden dark:bg-gray-800 dark:border-gray-700">
+            {/* <a href="#">
+              <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+            </a> */}
+            <div className="p-6">
+              <a>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{resource_entry.title}</h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 whitespace-normal break-words">{resource_entry.description}</p>
+              <a className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                {resource_entry.class}
+                <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" fill="none" viewBox="0 0 14 10">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                </svg>
+              </a>
+            </div>
+          </div>
+        ))}
       </div>
+      ) : null}
     </div>
-  ))}
-</div>
+    <div>
+      <button onClick={ toggleGuides } className="bg-blue-500 hover:bg-white-300 text-white py-2 px-4 rounded">Guides</button>
+      {guide ? (
+        <p>testing guides</p>
+      ) : null}
+    </div>
+    <div>
+      <button onClick= { togglePartnership } className="bg-red-500 hover:bg-white-300 text-white py-2 px-4 rounded">Partnerships</button>
+      {partnership ? (
+        <p>testing partnership</p>
+      ) : null}
+    </div>
+
+    <div>
+      <button onClick={ toggleDataset } className="bg-orange-500 hover:bg-white-300 text-white py-2 px-4 rounded">Datasets</button>
+      {dataset ? (
+        <p>testing dataset</p>
+      ) : null}
+    </div>
+
+
     </div>
     </div>
 
