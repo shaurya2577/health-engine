88c22cef7f1256aa9bbd5780e2285822fa38aa30
OLIVER YE <oliverye@berkeley.edu>
2024-01-04 12:23:46 -0800

    clean up index.js

--- server/index.js
+++ server/index.js
@@ -6,40 +6,56 @@ const pool = require("./db");
 
 const app = express();
 
-//DATABASE_URL=postgres://oliver_blog_backend:Akq3ThqbLJ1skJL@oliver-blog-backend-db.flycast:5432/oliver_blog_backend?sslmode=disable
-
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
 // create a new resource
-app.post('/newResource', async (req, res) => {
-  const { description } = req.body;
-  const { title } = req.body;
-  const { tag } = req.body;
-  const newResource = await pool.query("INSERT INTO resources (description, title, class) VALUES($1, $2, $3) RETURNING *", [description, title, tag]);
+app.post("/newResource", async (req, res) => {
+  const { description, title, tag } = req.body;
+  if (!description || !title || !tag) {
+    return res.status(400).send("Input is not formatted properly");
+  }
 
-  res.json(newResource.rows[0]);
-})
+  try {
+    const newResource = await pool.query(
+      "INSERT INTO resources (description, title, class) VALUES($1, $2, $3) RETURNING *;",
+      [description, title, tag]
+    );
 
+    res.json(newResource.rows[0]);
+  } catch (error) {
+    res.status(500).send("Server or database error");
+  }
+});
 
-// get all resources 
-app.get("/resources", async (req, res) => {
-  const allResources = await pool.query("SELECT * FROM resources");
-  res.json(allResources.rows);
-})
+// get all resources
+app.get("/resources", async (_req, res) => {
+  try {
+    const allResources = await pool.query("SELECT * FROM resources;");
+    res.json(allResources.rows);
+  } catch (error) {
+    res.status(500).send("Server or database error");
+  }
+});
 
 // see one resource
-app.get('/resources/:resourceID', async (req, res) => {
-  const { resourceID } = req.params;
-  const resource_fetch = await pool.query("SELECT * FROM resources WHERE resource_id = $1", [resourceID]);
-  res.json(resource_fetch.rows[0]);
-})
+app.get("/resources/:resourceID", async (req, res) => {
+  const { resource_id } = req.params;
+  if (!resource_id) {
+    return res.status(400).send("Input is not formatted properly");
+  }
 
-app.get("/", async(req, res) => {
-  res.send("Welcome to Health Engine Buddy");
-})
+  try {
+    const resource_fetch = await pool.query(
+      "SELECT * FROM resources WHERE resource_id = $1;",
+      [resource_id]
+    );
+    res.json(resource_fetch.rows[0]);
+  } catch (error) {
+    res.status(500).send("Server or database error");
+  }
+});
 
 app.listen(3002, () => {
   console.log("db listening on port 3002");
