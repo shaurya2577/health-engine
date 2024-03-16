caafdbb6cf566a839d47d7a05d50cb73ce5966fb
OLIVER YE <oliverye@berkeley.edu>
2024-03-16 15:18:25 -0700

    fix dev's shitty changes

--- server/db.js
+++ server/db.js
@@ -2,18 +2,18 @@ const Pool = require("pg").Pool;
 
 // for local testing, change user and password as needed
 //const pool = new Pool({
-//user: "postgres",
-//password: "1104AInnov8!",
-//host: "localhost",
-//port: 5433,
-//database: "resourcedatabase",
+//  user: "postgres",
+//  password: "1104AInnov8!",
+//  host: "localhost",
+//  port: 5433,
+//  database: "resourcedatabase",
 //});
 
 const pool = new Pool({
   user: "postgres",
-  password: "1104AInnov8!",
+  password: "oliverye77",
   host: "localhost",
-  port: 5433,
+  port: 5432,
   database: "resourcedatabase",
 });
 
