d9f4868d6760073329d95602cee23511f647dd2b
Dev Patel <devpatel@Devs-MacBook-Pro.local>
2024-06-11 22:52:26 -0700

    replaced reroute for verify_password

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -10,7 +10,7 @@ function Login({ setPasswordVerified }) {
     event.preventDefault();
     // Check if the password is correct
     try {
-      const response = await fetch("http://localhost:3002/verifylogin", {
+      const response = await fetch("/verifylogin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ password }),
